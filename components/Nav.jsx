c7736c7f3439809cf052de659d0ab3bfb86a8f66
Selim Sami <Selim.Genc@hhu.de>
2023-05-19 16:49:45 +0200

    added auth and db section. I recieve an error in my browser rn. Error code 400. I ll find an answers.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,18 +7,19 @@ import {signIn,signOut,useSession,getProviders} from "next-auth/react"
 
 const Nav = () => {
 
-  const isUserLogedIn = true;
+  const {data : session} = useSession();
 
   const [providers,setProviders] = useState(null);
 
   const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     }
+    setUpProviders();
   },[])
 
   return (
@@ -36,7 +37,7 @@ const Nav = () => {
 
         {/* Desktop Navigation */}
         <div className="sm:flex hidden">
-          {isUserLogedIn ? (
+          {session?.user ? (
             <div className="flex gap-3 md:gap-5">
               <Link href="/create-prompt" className="black_btn">
                 Create Post
@@ -48,7 +49,7 @@ const Nav = () => {
 
               <Link href="/profile">
                 <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -77,10 +78,10 @@ const Nav = () => {
 
         {/* Mobile Navigation */}
         <div className="sm:hidden flex relative">
-          {isUserLogedIn ? (
+          {session?.user ? (
             <div className="flex">
                <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
