93f879569afcd3e880157fba06ede8cb3fc71f62
Selim Sami <Selim.Genc@hhu.de>
2023-05-23 16:15:54 +0200

    added Form section and fixed bugs

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,15 @@
+import { connectToDB } from "@utils/database";
+
+export const POST = async (req,) => {
+    const {userId, prompt,tag} = await req.json();
+
+    try{
+        
+    }catch(error){
+
+    }
+
+
+
+
+}
