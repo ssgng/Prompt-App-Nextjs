61fe2e5ffe89b3914a22b665a7cca587def1a087
Selim Sami <Selim.Genc@hhu.de>
2023-05-23 17:13:50 +0200

    finished the project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,29 +1,28 @@
 import "@styles/globals.css";
-import Nav from "@/components/Nav";
-import Provider from "@/components/Provider";
 
-export const metadata= {
-    title: "Promptopia",
-    description: "Discover & Share AI Pormpts"
-}
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
-const RootLayout = ({children}) => {
-  return (
-    <html lang= "en">
-        <body>
-            <Provider>
-                <div className="main">
-                    <div className="gradient"/>
-                </div>
-                
-                <main className="app">
-                    <Nav/>
-                    {children}
-                </main>
-            </Provider>
-        </body>
-    </html>
-  )
-}
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-export default RootLayout
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
